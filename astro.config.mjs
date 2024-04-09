acaf7f2bc06bf0b988296c473b0d0800c8196b33
Shaun <shaun.skeen@icloud.com>
2024-04-09 19:41:53 +0100

    Tailwind and demo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://shaundotdigital.github.io',
-    base: 'shaundotdigital.github.io',
+  site: 'https://shaundotdigital.github.io',
+  base: 'shaundotdigital.github.io',
+  integrations: [tailwind()]
 });
